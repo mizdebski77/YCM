7c9b889528c7192b3f3a01a5ddca09b676fcdd9d
Marcin Izdebski <mizdebski123@gmail.com>
2024-06-21 16:58:05 +0200

    Framer motion added

--- src/app/features/informations.tsx
+++ src/app/features/informations.tsx
@@ -1,10 +1,18 @@
+"use client";
 import AI from '../Images/AI.svg';
 import List from '../Images/List.svg';
 import Bulb from '../Images/bulb.svg';
+import { motion } from "framer-motion"
 
 export const Informations = () => {
     return (
-        <div className=" max-w-[90%] bg-base-100 shadow-xl m-auto my-12 grid grid-cols-3 justify-center align-center rounded-3xl">
+        <motion.div
+            className=" max-w-[90%] bg-base-100 shadow-xl m-auto my-12 grid grid-cols-3 justify-center align-center rounded-3xl"
+            initial={{ y: '30%', opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            viewport={{ once: false }}
+            transition={{ duration: 0.7 }}
+        >
             <div className=" p-6 flex justify-center">
                 <img src={AI.src} alt='AI brain' className='max-w-20' />
                 <span className="card-title">Use <span className='text-green'>AI</span> to create perfect CV</span>
@@ -19,6 +27,6 @@ export const Informations = () => {
                 <img src={Bulb.src} alt='AI brain' className='max-w-20' />
                 <span className="card-title">Create your own  <span className='text-green'>CV</span>  template</span>
             </div>
-        </div>
+        </motion.div>
     )
 }
