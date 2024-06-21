36b7207ed400ae399d5f48a0838592f56c6e9687
Marcin Izdebski <mizdebski123@gmail.com>
2024-06-21 15:38:54 +0200

    Bg image added

--- src/app/features/hero.tsx
+++ src/app/features/hero.tsx
@@ -1,7 +1,20 @@
+import background from '../Images/hero.jpg';
+
 export const Hero = () => {
     return (
-        <div>
-            <h1 className="text-2xl">Stand Out in the job Market with our <span className="text-red">AI - Powered</span> CV Creator</h1>
-        </div>
+        <header style={{
+            backgroundImage: `url(${background})`, backgroundSize: 'cover', backgroundPosition: 'center', marginTop: '-80px', boxShadow: '11px -400px 197px 66px rgba(0, 0, 0, 0.95) inset',
+        }} className='min-h-screen flex justify-center mt-(-10)'>
+            <h1 className="text-7xl text-blue ">Stand Out in the job Market with our <span className="text-green">AI - Powered</span> CV Creator</h1>
+            <div className="flex items-center gap-5">
+                <button className="btn bg-green text-white btn-active hover:bg-lightgreen">
+                    Creat your CV with AI
+                </button>
+                <span className="text-xl text-blue">or</span>
+                <button className="btn bg-blue text-white hover:bg-lightblue ">
+                    Log-In to yout account
+                </button>
+            </div>
+        </header>
     )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,13 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    colors: {
+      green: "#57BD34",
+      blue: '#3D4680',
+      white: '#f7f7f7',
+      lightgreen: 'rgba(	87, 189, 52, 0.8)',
+      lightblue: 'rgba(61, 70, 128, 0.8)'
+    }
   },
   plugins: [require('daisyui'),],
 };
