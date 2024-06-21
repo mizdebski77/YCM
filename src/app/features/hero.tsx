a7d0148d22ec97c50b47ec2f3295c5dbd53fdfa7
Marcin Izdebski <mizdebski123@gmail.com>
2024-06-21 15:07:17 +0200

    Initial commit

---
+++ src/app/features/hero.tsx
@@ -0,0 +1,7 @@
+export const Hero = () => {
+    return (
+        <div>
+            <h1 className="text-2xl">Stand Out in the job Market with our <span className="text-red">AI - Powered</span> CV Creator</h1>
+        </div>
+    )
+}
