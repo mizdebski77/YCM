948d109335a82898b86c45cb61a2bf143e19d2eb
Marcin Izdebski <mizdebski123@gmail.com>
2024-06-24 11:00:55 +0200

    Media query to informations added

--- src/app/core/arrays.ts
+++ src/app/core/arrays.ts
@@ -3,7 +3,7 @@ import List from '../Images/List.svg';
 import Bulb from '../Images/bulb.svg';
 
 export const informationsData = [
-    { img: AI, preText: 'Use AI ', coloredText: 'AI', postText: 'to create perfect CV' },
-    { img: List, preText: '20 + CV Tem', coloredText: 'CV', postText: 'templates' },
-    { img: Bulb, preText: 'Create your own', coloredText: 'CV', postText: 'template' },
+    { img: AI, preText: 'Use  ', coloredText: ' AI', postText: 'to create perfect CV' },
+    { img: List, preText: '20 + ', coloredText: 'CV', postText: 'templates' },
+    { img: Bulb, preText: 'Create your own ', coloredText: 'CV', postText: 'template' },
 ]
--- src/app/features/informations.tsx
+++ src/app/features/informations.tsx
@@ -5,7 +5,7 @@ import { informationsData } from '../core/arrays';
 export const Informations = () => {
     return (
         <motion.div
-            className=" max-w-[90%] bg-base-100 shadow-xl m-auto my-12 grid grid-cols-3 justify-center align-center rounded-3xl"
+            className=" max-w-[90%] bg-base-100 shadow-xl m-auto my-12 grid lg:grid-cols-3 justify-center align-center rounded-3xl"
             initial={{ y: '30%', opacity: 0 }}
             whileInView={{ y: 0, opacity: 1 }}
             viewport={{ once: false }}
@@ -17,14 +17,14 @@ export const Informations = () => {
                     whileInView={{ scale: 1 }}
                     viewport={{ once: false }}
                     transition={{ duration: 0.7, delay: index * 0.1 }}
-                    className={`p-6 flex justify-center ${index % 2 === 0 ? ' ' : 'border-x-2 border-green'}`}>
+                    className={`p-6 flex justify-center items-center gap-4 ${index % 2 === 0 ? ' ' : 'lg:border-y-0 border-y-2 lg:border-x-2 border-green'}`}>
                     <img
                         src={information.img.src}
                         alt='AI brain'
-                        className='max-w-20'
+                        className='md:max-w-20 max-w-12 '
                     />
                     <span
-                        className="card-title">
+                        className="md:card-title text-md font-medium">
                         {information.preText}
                         <span className='text-green'>
                             {information.coloredText}
