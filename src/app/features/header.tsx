a662cf4b70a6591bf5db08a34e186702eca47081
Marcin Izdebski <mizdebski123@gmail.com>
2024-06-24 11:19:22 +0200

    Media query to header added

--- src/app/features/header.tsx
+++ src/app/features/header.tsx
@@ -8,9 +8,9 @@ export const Header = () => {
             backgroundImage: `url(${background.src})`,
             backgroundSize: 'cover',
             backgroundPosition: 'center',
-            boxShadow: 'inset 1008px -184px 116px 5px #f7f7f7'
+            boxShadow: 'inset 1008px -184px 116px 5px #f7f7f7',
         }}
-            className='min-h-[540px] p-14 flex justify-start'
+            className='md:min-h-[540px] md:p-14 px-12 pt-14 flex justify-start'
         >
             <div className='grid justify-center items-center gap-2 max-w-[980px]'>
                 <motion.h1
@@ -18,7 +18,7 @@ export const Header = () => {
                     whileInView={{ y: 0, opacity: 1 }}
                     viewport={{ once: false }}
                     transition={{ duration: 0.7 }}
-                    className="text-7xl text-blue text-center">
+                    className="lg:text-7xl sm:text-4xl text-3xl text-blue text-center">
                     Stand Out in the job Market with our
                     <span className="text-green">AI - Powered</span> CV Creator</motion.h1>
                 <motion.div
@@ -26,15 +26,15 @@ export const Header = () => {
                     whileInView={{ y: 0, opacity: 1 }}
                     viewport={{ once: false }}
                     transition={{ duration: 0.7 }}
-                    className="flex items-center justify-center gap-5">
+                    className="md:flex grid items-center justify-center md:gap-5">
                     <button
-                        className="btn btn-acce border-none bg-green text-white btn-active hover:bg-lightgreen text-lg font-medium">
+                        className="btn btn-acce border-none bg-green text-white btn-active hover:bg-lightgreen md:text-lg text-sm font-medium">
                         Creat your CV with AI
                     </button>
-                    <div
-                        className="divider divider-horizontal">OR</div>
+                    <span
+                        className="divider divider-horizontal md:m-4 my-0 mx-auto">OR</span>
                     <button
-                        className="btn bg-blue border-none text-white hover:bg-lightblue text-lg font-medium ">
+                        className="btn bg-blue border-none text-white hover:bg-lightblue md:text-lg text-sm font-medium ">
                         Log-In to yout account
                     </button>
                 </motion.div>
