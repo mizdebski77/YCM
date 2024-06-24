fce8a49b117d3e812b1e8a6cd9dda011466a900f
Marcin Izdebski <mizdebski123@gmail.com>
2024-06-24 16:13:08 +0200

    Buble footer added

--- src/app/features/footer.tsx
+++ src/app/features/footer.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import logo from '../Images/whiteLogo.svg';
-
+import buble from '../Images/footerBuble.svg'
 export const Footer = () => {
     return (
         <footer className="footer footer-center bg-blue text-primary-content p-8">
+            <img src={buble.src} alt='footer buble' className='absolute right-0' />
             <aside className='border-b w-full p-4 pb-12'>
                 <img src={logo.src} alt='logo' className='md:max-w-80 max-w-32' />
 
