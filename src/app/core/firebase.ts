ce45aca2b327ba1d90405f3b880550d1c6e64340
Marcin Izdebski <mizdebski123@gmail.com>
2024-06-27 12:30:35 +0200

    Firebase config added

---
+++ src/app/core/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDaym7LHwnaZnyDje2ZtHfYN7F2RQEoCoQ",
+    authDomain: "ycm---your-cv-maker.firebaseapp.com",
+    projectId: "ycm---your-cv-maker",
+    storageBucket: "ycm---your-cv-maker.appspot.com",
+    messagingSenderId: "213804910441",
+    appId: "1:213804910441:web:ab5b81271e13ee72619ff8"
+};
+
+const app = initializeApp(firebaseConfig);
