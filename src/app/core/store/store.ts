facb6818714e1258ef9abf6695ae241ee192a818
Marcin Izdebski <mizdebski123@gmail.com>
2024-07-03 14:06:03 +0200

    Simple counter added

--- src/app/core/store/store.ts
+++ src/app/core/store/store.ts
@@ -1,13 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit'
+// store.ts
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from '../../counterslice';
 
 export const makeStore = () => {
     return configureStore({
-        reducer: {}
-    })
-}
+        reducer: {
+            counter: counterReducer,
+        },
+    });
+};
 
 // Infer the type of makeStore
-export type AppStore = ReturnType<typeof makeStore>
+export type AppStore = ReturnType<typeof makeStore>;
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore['getState']>
-export type AppDispatch = AppStore['dispatch']
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
+'use client'
 import { Header } from "./Home/header";
 import { HowItWorks } from "./Home/howItWorks";
 import { Informations } from "./Home/informations";
 import { OurCreator } from "./Home/ourCreator";
 import { Path } from "./Home/path";
 import { PhotoSection } from "./Home/photoSection";
+import { useSelector, useDispatch } from 'react-redux';
+import { AppDispatch, RootState } from "./core/store/store";
+import { decrement, increment, selectCount } from "./counterslice";
 
 export default function Home() {
+    const count = useSelector((state: RootState) => selectCount(state));
+    const dispatch = useDispatch<AppDispatch>();
+
     return (
         <main className='overflow-x-hidden '>
             <Header />
@@ -14,6 +21,12 @@ export default function Home() {
             <OurCreator />
             <Path />
             <PhotoSection />
+
+            <div>
+                <button onClick={() => dispatch(increment())}>+</button>
+                <span>{count}</span>
+                <button onClick={() => dispatch(decrement())}>-</button>
+            </div>
         </main>
     )
 }
