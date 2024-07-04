d6895dff463c8592172ae5335a60939493fee29f
Marcin Izdebski <mizdebski123@gmail.com>
2024-07-04 13:36:46 +0200

    Profile basics data added

--- src/app/Profile/page.tsx
+++ src/app/Profile/page.tsx
@@ -2,6 +2,11 @@
 import { useEffect, useState } from "react";
 import { auth, db } from "../core/firebase";
 import { doc, getDoc } from "firebase/firestore";
+import { MdEmail } from "react-icons/md";
+import account from '../common/Images/account.jpg';
+import { IoPersonOutline } from "react-icons/io5";
+import { MdOutlinePersonAddAlt } from "react-icons/md";
+import { FaCity } from "react-icons/fa";
 
 interface UserDetails {
     name: string;
@@ -44,16 +49,62 @@ export default function Profile() {
     }
 
     return (
-        <div className="min-h-screen">
-            {userDetails ? (
-                <>
-                    <div>Profil użytkownika</div>
-                    <span>{userDetails.name}</span>
-                    <button className="btn" onClick={handleLogout}>Log Out</button>
-                </>
-            ) : (
-                <p>Loading...</p>
-            )}
+        <div className="min-h-screen max-w-5xl mx-auto py-24">
+            <h1 className="text-center text-5xl border-b-2 pb-6 text-green">Account</h1>
+            <div className=' shadow-[0_0_20px_#b9b9b9] rounded-2xl md:p-14 p-8 mt-12 mx-auto '>
+                {userDetails ? (
+                    <div className=" grid grid-cols-[1fr,2fr]">
+                        <div className="max-w-52   rounded-full sticky overflow-hidden shadow-[0_0_20px_#b9b9b9]">
+                            <img src={account.src} alt="account" className="m-auto w-full relative" />
+                            {/* <div className="border-r-2">{userDetails.name}</div> */}
+
+                        </div>
+
+                        <div className="grid grid-cols-2 gap-4 px-4">
+                            <label className="input input-bordered flex items-center gap-2">
+                                <IoPersonOutline />
+                                <input
+                                    type="name"
+                                    className="grow"
+                                    placeholder="Name"
+                                />
+                            </label>
+
+                            <label className="input input-bordered flex items-center gap-2">
+                                <MdOutlinePersonAddAlt />
+                                <input
+                                    type="name"
+                                    className="grow"
+                                    placeholder="Last Name"
+                                />
+                            </label>
+
+                            <label className="input input-bordered flex items-center gap-2">
+                                <FaCity />
+                                <input
+                                    type="text"
+                                    className="grow"
+                                    placeholder="City"
+                                />
+                            </label>
+
+                            <label className="input input-bordered flex items-center gap-2">
+                                <MdEmail />
+                                <input
+                                    type="email"
+                                    className="grow"
+                                    placeholder="Email"
+                                />
+                            </label>
+
+
+                        </div>
+                    </div>
+                ) : (
+                    <p>Loading...</p>
+                )}
+            </div>
         </div>
+
     );
 }
