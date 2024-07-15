236402a6924a73814ccc8354af56c858e2879f5d
Marcin Izdebski <mizdebski123@gmail.com>
2024-07-15 16:22:17 +0200

    Profile to navigation added

--- src/app/common/navigation.tsx
+++ src/app/common/navigation.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { auth, db } from "../core/firebase";
 import { doc, getDoc } from "firebase/firestore";
 import { UserDetails } from '../core/intrface';
+import profile from './Images/account.svg';
 
 export const Navigation = () => {
     const [userDetails, setUserDetails] = useState<UserDetails | null>(null);
@@ -30,6 +31,9 @@ export const Navigation = () => {
         fetchUserData();
     }, []);
 
+    console.log(userDetails);
+
+
     return (
         <nav className="navbar bg-[#f7f7f7] sticky top-0 z-20 px-12 py-4 flex justify-between">
             <div className="flex justify-between ">
@@ -37,10 +41,10 @@ export const Navigation = () => {
                     <img src={logo.src} alt='logo' />
                 </a>
             </div>
-
             <div>
-                <a>
-                    Profile
+                <a className='flex items-center gap-3' href='/Profile'>
+                    <img src={profile.src} className='max-w-8' alt='profile' />
+                    {userDetails ? userDetails.name : ' '}
                 </a>
             </div>
         </nav>
