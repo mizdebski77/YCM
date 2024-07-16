c200cab10160beba32b2a0637fe31ac52aea17e5
Marcin Izdebski <mizdebski123@gmail.com>
2024-07-16 15:29:03 +0200

    Deploy test 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
 'use client'
-// import { HowItWorks } from "./Home/howItWorks";
-// import { OurCreator } from "./Home/ourCreator";
-// import { Path } from "./Home/path";
-    // import { PhotoSection } from "./Home/photoSection";
 import { useSelector, useDispatch } from 'react-redux';
 import { AppDispatch, RootState } from "./core/store/store";
 import { decrement, increment, selectCount } from "./counterslice";
-// import { Informations } from "./Home/informations";
-// import Header from "./Home/header";
-import LogIn from "./Log-In/page";
-import Header from './Home/header';
+import Hero from './Home/page';
 
 export default function Home() {
     const count = useSelector((state: RootState) => selectCount(state));
@@ -17,12 +10,7 @@ export default function Home() {
 
     return (
         <main className='overflow-x-hidden '>
-            <Header />
-            {/* <Informations />
-            <HowItWorks />
-            <OurCreator />
-            <Path />
-            <PhotoSection /> */}
+            <Hero />
             <div>
                 <button onClick={() => dispatch(increment())}>+</button>
                 <span>{count}</span>
